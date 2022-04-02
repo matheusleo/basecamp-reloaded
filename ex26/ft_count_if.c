512c32abd0817de4f390089708b0046745c13594
matheusleo <matheusleotrab@gmail.com>
2022-04-02 20:36:30 -0300

    fix ex05 and ex26
    fix ex05 and ex26 after the Moulinette tests failed

--- ex26/ft_count_if.c
+++ ex26/ft_count_if.c
@@ -6,7 +6,7 @@
 /*   By: mleonard <mleonard@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/02 15:38:26 by mleonard          #+#    #+#             */
-/*   Updated: 2022/04/02 16:00:41 by mleonard         ###   ########.fr       */
+/*   Updated: 2022/04/02 20:34:23 by mleonard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,13 @@ int	ft_count_if(char **tab, int (*f)(char*))
 	int	index;
 	int	if_counter;
 
-	index = -1;
+	index = 0;
 	if_counter = 0;
-	while (tab[++index][0] != '0')
+	while (tab[index])
 	{
 		if (f(tab[index]) == 1)
 			if_counter++;
+		index++;
 	}
 	return (if_counter);
 }
